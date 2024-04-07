31c1a20f6d05299aba2d89504a5fe49e448b20eb
dominhvu2605 <dominhvu2605@gmail.com>
2024-04-07 16:53:40 +0700

    #2: BE - first commit

---
+++ back-end/spring-boot-library/src/main/java/com/library/springbootlibrary/entity/Book.java
@@ -0,0 +1,35 @@
+package com.library.springbootlibrary.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "book")
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "Description")
+    private String Description;
+
+    @Column(name = "copies")
+    private String copies;
+
+    @Column(name = "copies_available")
+    private String copiesAvailable;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "img")
+    private String img;
+}
