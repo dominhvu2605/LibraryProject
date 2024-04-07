2a4f01cd1c5969b2884d788fac1e6069690c072e
dominhvu2605 <dominhvu2605@gmail.com>
2024-04-07 16:49:50 +0700

    #1: FE - first commit

---
+++ front-end/src/models/BookModel.ts
@@ -0,0 +1,32 @@
+class BookModel {
+  id: number;
+  title: string;
+  author?: string;
+  description?: string;
+  copies?: number;
+  copiesAvailabel?: number;
+  category?: number;
+  img?: string;
+
+  constructor(
+    id: number,
+    title: string,
+    author: string,
+    description: string,
+    copies: number,
+    copiesAvailabel: number,
+    category: number,
+    img: string
+  ) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.description = description;
+    this.copies = copies;
+    this.copiesAvailabel = copiesAvailabel;
+    this.category = category;
+    this.img = img;
+  }
+}
+
+export default BookModel;
