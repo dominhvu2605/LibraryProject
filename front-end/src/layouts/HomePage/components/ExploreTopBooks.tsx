db425807d41e575718cf9f503c31e8f7dfe7bf16
dominhvu2605 <dominhvu2605@gmail.com>
2024-04-15 21:44:01 +0700

    #5: using react-router-dom

--- front-end/src/layouts/HomePage/components/ExploreTopBooks.tsx
+++ front-end/src/layouts/HomePage/components/ExploreTopBooks.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 export const ExploreTopBooks = () => {
   return (
     <div className="p-5 mb-4 bg-dark header">
@@ -8,13 +10,13 @@ export const ExploreTopBooks = () => {
         <div>
           <h1 className="display-5 fw-bold">Find your text adventure</h1>
           <p className="col-md-8 fs-4">Where would you like to go next</p>
-          <a
+          <NavLink
             type="butotn"
             className="btn main-color btn-lg text-white"
-            href="#"
+            to="/search"
           >
             Explore top books
-          </a>
+          </NavLink>
         </div>
       </div>
     </div>
